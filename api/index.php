b155f65e73de91503b78fa2d178e4190b4f9c023
basmaelmaimouni <basmaelmaimouni6@gmail.com>
2026-04-03 01:05:15 +0100

    first commit creation de index.php

---
+++ api/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WELCOME TO MY PORTFOLIO</h1>
+
+	<?php
+	$groupe="dev101";
+	$plt="vercel";
+
+	echo("<h2>premier site de $groupe sur $plt");
+	
+	
+	?>
+    
+</body>
+</html>
